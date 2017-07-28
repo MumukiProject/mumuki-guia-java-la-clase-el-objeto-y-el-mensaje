b67704e3a888d6e69625766be04f75d2708d0309
franco@mumuki.org <franco@mumuki.org>
2017-07-28 17:08:51 -0400

    Mumuki Export on 2017-07-28 17:08:51 -0400

--- 00003_Unos tipos muy primitivos/test.java
+++ 00003_Unos tipos muy primitivos/test.java
@@ -26,6 +26,6 @@ public void si_la_golondrina_come_10g_de_alpiste_y_tiene_200_de_energia_queda_en
 public void si_la_golondrina_come_24g_de_alpiste_y_tiene_300_de_energia_queda_en_310() {
   Golondrina g = new Golondrina();
   g.energia = 300;
-  g.comerAlpise(12);
+  g.comerAlpiste(12);
   Assert.assertEquals(324, g.energia);
 }
