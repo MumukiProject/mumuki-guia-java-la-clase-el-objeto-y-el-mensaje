bc7e1b09966c814af71d19cd5a98c3ec728bfc7c
franco@mumuki.org <franco@mumuki.org>
2017-07-28 15:27:28 -0400

    Mumuki Export on 2017-07-28 15:27:28 -0400

--- 00002_¿Con qué se come?/test.java
+++ 00002_¿Con qué se come?/test.java
@@ -3,7 +3,7 @@ public void si_la_golondrina_vuela_en_circulos_y_tiene_200_de_energia_queda_en_1
   Golondrina g = new Golondrina();
   g.energia = 200;
   g.volarEnCirculos();
-  Assert.assertEquals(100);
+  Assert.assertEquals(100, g.energia);
 }
 
 @Test
@@ -11,7 +11,7 @@ public void si_la_golondrina_vuela_en_circulos_y_tiene_300_de_energia_queda_en_2
   Golondrina g = new Golondrina();
   g.energia = 300;
   g.volarEnCirculos();
-  Assert.assertEquals(200);
+  Assert.assertEquals(200, g.energia);
 }
 
 @Test
@@ -19,7 +19,7 @@ public void si_la_golondrina_come_alpiste_y_tiene_200_de_energia_queda_en_210() 
   Golondrina g = new Golondrina();
   g.energia = 200;
   g.volarEnCirculos();
-  Assert.assertEquals(210);
+  Assert.assertEquals(210, g.energia);
 }
 
 @Test
@@ -27,5 +27,5 @@ public void si_la_golondrina_come_alpiste_y_tiene_300_de_energia_queda_en_310() 
   Golondrina g = new Golondrina();
   g.energia = 300;
   g.volarEnCirculos();
-  Assert.assertEquals(310);
+  Assert.assertEquals(310, g.energia);
 }
