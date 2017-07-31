066b90ffdd37ebb52917653045b91d9953d63aa0
franco@mumuki.org <franco@mumuki.org>
2017-07-31 17:05:22 -0400

    Mumuki Export on 2017-07-31 17:05:22 -0400

--- 00008_Peso a Peso/test.java
+++ 00008_Peso a Peso/test.java
@@ -1,1 +1,26 @@
-@Test  
+@Test
+public void se_puede_ejecutar_el_programa() {
+  Main.main(new String[]{});
+}
+
+@Test
+public void reiniciar_vuelve_a_cero_la_cantidadDeClientes() {
+  Supermercado superJimenez = new Supermercado();
+  
+  superJimenez.registrarVenta(21);
+  superJimenez.registrarVenta(30);
+  
+  superJimenez.reiniciar();
+
+  Assert.assertEquals(0, superJimenez.cantidadDeClientes());
+}
+
+@Test
+public void registrarVenta_aumenta_la_cantidadDeClientes() {
+  Supermercado superJimenez = new Supermercado();
+  
+  superJimenez.registrarVenta(21);
+  superJimenez.registrarVenta(30);
+  
+  Assert.assertEquals(2, superJimenez.cantidadDeClientes());
+}
