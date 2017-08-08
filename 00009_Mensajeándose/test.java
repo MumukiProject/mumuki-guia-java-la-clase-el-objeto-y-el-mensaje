1394ceca718fc2ac47a922ce30794d905a53f3ce
franco@mumuki.org <franco@mumuki.org>
2017-08-08 11:13:53 -0400

    Mumuki Export on 2017-08-08 11:13:53 -0400

--- 00009_Mensajeándose/test.java
+++ 00009_Mensajeándose/test.java
@@ -5,12 +5,12 @@ Ciudad rosario = new Ciudad();
 public void si_esta_en_rosario_la_distancia_a_buenos_aires_es_400() {
   Golondrina pepita = new Golondrina();
   pepita.ubicacion = buenosAires;
-  Assert.assertEquals(400, pepita.distanciaA(rosario));
+  Assert.assertEquals(400, pepita.distanciaA(rosario), 0.1);
 }
 
 @Test
 public void si_esta_en_buenosAires_la_distancia_a_rosario_es_400() {
   Golondrina pepita = new Golondrina();
   pepita.ubicacion = rosario;
-  Assert.assertEquals(400, pepita.distanciaA(buenosAires));
+  Assert.assertEquals(400, pepita.distanciaA(buenosAires), 0.1);
 }
