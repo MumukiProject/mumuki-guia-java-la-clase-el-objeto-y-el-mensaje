25042314fe5547774065f5deb5414a12e808a94e
franco@mumuki.org <franco@mumuki.org>
2017-08-15 11:17:12 -0400

    Mumuki Export on 2017-08-15 11:17:12 -0400

--- 00018_Más libros/test.java
+++ 00018_Más libros/test.java
@@ -1,21 +1,16 @@
-
 @Test
-public void un_libro_de_1490_con_colofon_es_incunable() {
-  Libro libro = new Libro(1490, true);
-  Assert.assertTrue(libro.esIncunable());
+public void se_puede_crear_a_alejandra_y_enviarle_getCantidadLibrosRevisados() {
+  Bibliotecario alejandra = new Bibliotecario(1200);
+  Assert.assertEquals("La cantidad inicial de libros revisados de alejandra es 1200", 1200, alejandra.getCantidadLibrosRevisados());
 }
 
 @Test
-public void un_libro_de_1590_sin_colofon_es_incunable() {
-  Libro libro = new Libro(1590, false);
-  Assert.assertTrue(libro.esIncunable());
+public void se_puede_crear_a_una_bibliotecaria_con_200_libros_revisados_y_enviarle_getCantidadLibrosRevisados() {
+  Bibliotecario maria = new Bibliotecario(200);
+  Assert.assertEquals("La cantidad inicial de libros revisados de maria es 200", 200, maria.getCantidadLibrosRevisados());
 }
 
 @Test
-public void un_libro_de_1629_sin_colofon_es_incunable() {
-  Libro libro = new Libro(1629, false);
-  Assert.assertTrue(libro.esIncunable());
-}TODO@Test
 public void un_libro_de_1490_con_colofon_es_incunable() {
   Libro libro = new Libro(1490, true);
   Assert.assertTrue(libro.esIncunable());
