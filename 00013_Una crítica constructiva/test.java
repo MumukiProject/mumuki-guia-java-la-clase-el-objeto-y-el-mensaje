bc03554614a108b3a8b00259e73b62cac6d268b1
franco@mumuki.org <franco@mumuki.org>
2017-08-15 10:00:53 -0400

    Mumuki Export on 2017-08-15 10:00:53 -0400

--- 00013_Una crítica constructiva/test.java
+++ 00013_Una crítica constructiva/test.java
@@ -8,8 +8,6 @@ public void un_libro_de_1490_con_colofon_es_incunable() {
 @Test
 public void un_libro_de_1590_sin_colofon_es_incunable() {
   Libro libro = new Libro(1590, false);
-  libro.anioEdicion = ;
-  libro.tieneColofon = ;
   Assert.assertTrue(libro.esIncunable());
 }
 
