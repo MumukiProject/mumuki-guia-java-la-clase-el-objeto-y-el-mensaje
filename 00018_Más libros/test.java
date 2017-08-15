b1392af8946c427d8f8f6ef58f1c65bec6dcc32b
franco@mumuki.org <franco@mumuki.org>
2017-08-15 11:16:29 -0400

    Mumuki Export on 2017-08-15 11:16:29 -0400

--- 00018_Más libros/test.java
+++ 00018_Más libros/test.java
@@ -1,14 +1,3 @@
-@Test
-public void se_puede_crear_a_alejandra_y_enviarle_getCantidadLibrosRevisados() {
-  Bibliotecario alejandra = new Bibliotecario(1200);
-  Assert.assertEquals("La cantidad inicial de libros revisados de alejandra es 1200", 1200, alejandra.getCantidadLibrosRevisados());
-}
-
-@Test
-public void se_puede_crear_a_una_bibliotecaria_con_200_libros_revisados_y_enviarle_getCantidadLibrosRevisados() {
-  Bibliotecario maria = new Bibliotecario(200);
-  Assert.assertEquals("La cantidad inicial de libros revisados de maria es 200", 200, maria.getCantidadLibrosRevisados());
-}
 
 @Test
 public void un_libro_de_1490_con_colofon_es_incunable() {
