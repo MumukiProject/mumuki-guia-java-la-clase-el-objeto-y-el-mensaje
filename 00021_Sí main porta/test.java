9768dd8848c0de5701073494e9f53ee420ffb52a
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2019-01-04 16:37:39 -0500

    Mumuki Export on 2019-01-04 16:37:39 -0500

--- 00021_Sí main porta/test.java
+++ 00021_Sí main porta/test.java
@@ -1,12 +1,4 @@
 @Test
-public void al_ejecutar_el_programa_el_volumen_esta_en_100() {
+public void se_puede_ejecutar_el_programa() {
   Fiesta.main(new String[]{});
-  Assert.assertEquals("El volumen debe ser 100 al ejecutar el programa", 100, fiesta.volumen);
-  
-}
-
-@Test
-public void al_ejecutar_el_programa_las_puertas_estan_abiertas() {
-  Fiesta.main(new String[]{});
-  Assert.assertTrue("Las puertas deben abrirse la ejecutar el programa", puertasAbiertas);
 }
